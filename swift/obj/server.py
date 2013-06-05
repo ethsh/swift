67ddbe30fb04c0f03b638589b924e021df4e375c
ethsh <et.shiloh@gmail.com>
2013-06-05 22:22:50 +0300

    Deleted the body change to torrent data

--- swift/obj/server.py
+++ swift/obj/server.py
@@ -1195,7 +1195,7 @@ class ObjectController(object):
         response.headers['torrent'] = bencode(make_meta_files(ip, [file.data_file]))
         response.headers['torrent_length'] = len(response.headers['torrent'])
         save_as = file.data_file
-        response.body = bencode(make_meta_files(ip, [file.data_file]))
+        # response.body = bencode(make_meta_files(ip, [file.data_file]))
         # self.seeder_thread = SeederThread(ip, save_as, bencode(make_meta_files(ip, [file.data_file])))
         # response.headers['torrent'] = response.headers['torrent']
         # self.seeder_thread.start()
